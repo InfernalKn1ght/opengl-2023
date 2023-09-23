3e69208efb370f0705e55f7ab73d9d959921065f
InfernalKn1ght <arzhaev.dmitry@yandex.ru>
2023-09-23 13:19:30 +0300

    2.1

--- 2023.04.11-OpenGL-Test-Project1/2023.04.11-OpenGL-Test-Project1/Source.cpp
+++ 2023.04.11-OpenGL-Test-Project1/2023.04.11-OpenGL-Test-Project1/Source.cpp
@@ -4,16 +4,20 @@
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
 int main(int argc, char* argv[]) {
+
 	glfwInit(); //Инициализация GLFW
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); //Версия
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // version
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //?
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); //?
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // ?
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); // ?
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "TestWindow", nullptr, nullptr); //Создание объекта окна
+	GLFWwindow* window = glfwCreateWindow(800, 600, "TestWindow", nullptr, nullptr); // window object creating
 	if (window == nullptr)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -34,13 +38,30 @@ int main(int argc, char* argv[]) {
 
 	glViewport(0, 0, width, height);
 
-	while (!glfwWindowShouldClose(window))
+	glfwSetKeyCallback(window, key_callback);
+
+	while (!glfwWindowShouldClose(window)) // game cycle
 	{
 		glfwPollEvents();
+
+		// render
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // clear color buffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 	}
 
 	glfwTerminate();
 
 	return 0;
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// when user presses escape, WindowShouldClose sets to true
+	// and then app closes
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
